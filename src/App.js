2897fdcbb0f6718eb8807f115f1d56e5ef36d415
QaziSafiya <safiyaqazi517@gmail.com>
2023-10-07 12:31:33 +0530

    set tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center">
+      <div className="w-[200px] h-[120px] p-4 m-5  bg-slate-400 md:bg-red-500 lg:bg-green-200 sm:bg-orange-300 ">
+        Box1
+      </div>
+      <div className="w-[200px] h-[120px] p-4 m-5  bg-green-400 md:bg-red-500 lg:bg-green-200 sm:bg-orange-300 ">
+        box2
+      </div>
+      <div className="w-[200px] h-[120px] p-4 m-5  bg-red-400 md:bg-red-500  lg:bg-green-200 sm:bg-orange-300 ">
+        box3
+      </div>
+      <div className="w-[200px] h-[120px] p-4 m-5  bg-blue-400 md:bg-red-500  lg:bg-green-200 sm:bg-orange-300 ">
+        box4
+      </div>
     </div>
   );
 }
